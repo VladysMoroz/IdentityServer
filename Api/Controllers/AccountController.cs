afb3ce8924ec0767019d9ada7b73108bd2fdc692
Vladymor <vladusmoroz@gmail.com>
2025-03-17 18:38:45 +0200

    Added controllers login and register Added JWT Service for creating JWT-keys Added Admin Seeder in Database Added basic IdentityServer Configuration Added class Config with ApiScope, ApiRecources, Clients

---
+++ Api/Controllers/AccountController.cs
@@ -0,0 +1,96 @@
+﻿using Api.DTOs.Account;
+using Api.Entities;
+using Api.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly JWTService _jwtService;
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+
+        public AccountController(JWTService jwtService,
+            SignInManager<User> signInManager,
+            UserManager<User> userManager)
+        {
+            _jwtService = jwtService;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [Authorize]
+        [HttpGet("refresh-user-token")]
+        public async Task<ActionResult<UserDto>> RefreshUserToken()
+        {
+            var user = await _userManager.FindByNameAsync(User.FindFirst(ClaimTypes.Email)?.Value);
+            return await CreateApplicationUserDto(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user == null) return Unauthorized("Invalid UserName or Password");
+
+            if (user.EmailConfirmed == false) return Unauthorized("Please confirm your email");
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+
+            if (!result.Succeeded) return Unauthorized("Invalid Username or Password");
+
+            return await CreateApplicationUserDto(user);
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto model)
+        {
+            if(await CheckEmailExistsAsync(model.Email))
+            {
+                return BadRequest($"An existing account is using {model.Email} email adress. Please try with another email adress");
+            }
+
+            var userToAdd = new User
+            {
+                FirstName = model.FirstName.ToLower(),
+                LastName = model.LastName.ToLower(),
+                UserName = model.Email.ToLower(),
+                Email = model.Email.ToLower(),
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(userToAdd, model.Password);
+
+            if (!result.Succeeded) return BadRequest(result.Errors);
+
+            await _userManager.AddToRoleAsync(userToAdd, "User");
+
+            return Ok("Your account has been created successfully, you can login");
+        }
+
+        #region Private Helper Methods
+        private async Task<UserDto> CreateApplicationUserDto(User user)
+        {
+            return new UserDto
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                JWT = await _jwtService.CreateJWT(user)
+            };
+        }
+
+        private async Task<bool> CheckEmailExistsAsync(string email)
+        {
+            return await _userManager.Users.AnyAsync(x => x.Email == email.ToLower());
+        }
+        #endregion
+    }
+}
--- Api/Program.cs
+++ Api/Program.cs
@@ -1,13 +1,104 @@
+using Api;
+using Api.Database;
+using Api.Entities;
+using Api.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<Context>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+//builder.Services.AddIdentityServer()
+//             .AddDeveloperSigningCredential()
+//             .AddInMemoryApiResources(Config.GetApiResources())
+//             .AddInMemoryClients(Config.GetClients());
+
+builder.Services.AddIdentityServer()
+     .AddDeveloperSigningCredential()
+     .AddInMemoryApiResources(Config.GetApiResources())  // Повертає API ресурси
+     .AddInMemoryApiScopes(Config.GetApiScopes())  // Повертає API scopes
+     .AddInMemoryClients(Config.GetClients())  // Повертає клієнтів
+     .AddInMemoryIdentityResources(Config.GetIdentityResources());  // Повертає ресурси ідентифікації (якщо потрібно)
+
+// be able to inject JWTService class inside our Controllers
+builder.Services.AddScoped<JWTService>();
+
+// defining our IdentityCore Service
+builder.Services.AddIdentityCore<User>(options =>
+{
+    // password configuration
+    options.Password.RequiredLength = 6;
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+
+    // for email configuration
+    options.SignIn.RequireConfirmedEmail = true;
+})
+    .AddRoles<IdentityRole>() // be able to add roles
+    .AddRoleManager<RoleManager<IdentityRole>>() // be able to make use of RoleManager
+    .AddEntityFrameworkStores<Context>() // providing our context
+    .AddSignInManager<SignInManager<User>>() // make use of SignIn manager
+    .AddUserManager<UserManager<User>>() // make use of UserManager to create users
+    .AddDefaultTokenProviders(); // be able to create tokens for email confirmation
+
+// be able to authenticate users using JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            // validate the token based on the key we have provided inside appsettings.development.json JWT:KEy
+            ValidateIssuerSigningKey = true,
+            // the issuer signing key based on JWT:Key
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"])),
+            // the issuer which in here is the api project url we are using
+            ValidIssuer = builder.Configuration["JWT:Issuer"],
+            // validate the issuer ( who ever is issuing the JWT )
+            ValidateIssuer = true,
+            // don't validate audience ( angular side )
+            ValidateAudience = false,
+            ValidAudience = builder.Configuration["JWT:Audience"]
+        };
+    });
+
+
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminPolicy", policy => policy.RequireRole("Admin"));
+    options.AddPolicy("UserPolicy", policy => policy.RequireRole("User"));
+
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,8 +107,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseIdentityServer();
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
